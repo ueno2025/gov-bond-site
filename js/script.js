55aa77780b70d503655f5271be1e7b31439a1505
ueno2025 <ken825325@outlook.jp>
2026-08-11 21:17:14 +0900

    CSVファイル追加

---
+++ js/script.js
@@ -0,0 +1,23 @@
+fetch("csv/kokusai5.csv")
+    .then(response => response.text())
+    .then(data => {
+
+        const rows = data.trim().split("\n");
+        const tableBody = document.getElementById("table-body");
+
+        // 1行目（ヘッダー）を除外
+        rows.slice(1).forEach(row => {
+
+            const columns = row.split(",");
+
+            const tr = document.createElement("tr");
+
+            columns.forEach(column => {
+                const td = document.createElement("td");
+                td.textContent = column;
+                tr.appendChild(td);
+            });
+
+            tableBody.appendChild(tr);
+        });
+    });
